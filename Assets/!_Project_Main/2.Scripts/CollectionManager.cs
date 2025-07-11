5543ab5d7e633fd37f65564b72cc8fc00e8470ea
Lee Soohyeon <111167280+LeeSoohyeon-dev@users.noreply.github.com>
2025-07-11 18:36:48 +0900

    feat : 아이템 수집 기능 프로토타입
    - 아이템을 그랩한 상태에서 트리거 or c버튼 누르면 수집
    - 향후 인벤토리로 변경할 예정

---
+++ Assets/!_Project_Main/2.Scripts/CollectionManager.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+public class CollectionManager : MonoBehaviour
+{
+public static CollectionManager Instance;
+
+    public bool hasWallet = false;
+    public bool hasFlashlight = false;
+    public bool hasLaptop = false;
+    public bool hasSmartphone = false;
+    
+    public bool AllItemsCollected => hasWallet && hasFlashlight && hasLaptop && hasSmartphone;
+    
+    void Awake()
+    {
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    public void CollectItem(ItemType itemType)
+    {
+        switch (itemType)
+        {
+            case ItemType.Wallet:
+                hasWallet = true;
+                Debug.Log("지갑 수집!");
+                break;
+                
+            case ItemType.Flashlight:
+                hasFlashlight = true;
+                Debug.Log("손전등 수집!");
+                break;
+                
+            case ItemType.Laptop:
+                hasLaptop = true;
+                Debug.Log("랩탑 수집!");
+                break;
+                
+            case ItemType.Smartphone:
+                hasSmartphone = true;
+                Debug.Log("스마트폰 수집!");
+                break;
+        }
+        
+        CheckCollectionComplete();
+    }
+    
+    void CheckCollectionComplete()
+    {
+        if (AllItemsCollected)
+        {
+            Debug.Log("모든 아이템 수집 완료");
+            OnAllItemsCollected();
+        }
+    }
+    
+    void OnAllItemsCollected()
+    {
+        // 모든 아이템 수집 완료 시 처리(문 열림?)
+    }
+    
+    public bool IsItemCollected(ItemType itemType)
+    {
+        return itemType switch
+        {
+            ItemType.Wallet => hasWallet,
+            ItemType.Flashlight => hasFlashlight,
+            ItemType.Laptop => hasLaptop,
+            ItemType.Smartphone => hasSmartphone,
+            _ => false
+        };
+    }
+}
